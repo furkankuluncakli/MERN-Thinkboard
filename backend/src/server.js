28447c25ff52135b7d74a34064745539c059c063
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-08-31 17:20:53 +0300

    Add Express server and notes API routes with CRUD operations

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import router from "./routes/notesRoutes.js";
 
 const app = express();
+
+app.use("/api/notes", router)
 
 app.listen(5001, () => {
   console.log("Server started on PORT:5001");
