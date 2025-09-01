4fcb677c543e1fe49d91fcfe18bcf59c9b05fd10
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-09-01 18:40:02 +0300

    feat: implement NoteCard component and integrate it into HomePage; add formatDate utility function

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../components/Navbar";
-import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
+import NoteCard from "../components/NoteCard"
 import RateLimitedUI from "../components/RateLimitedUI";
 
 function HomePage() {
@@ -35,9 +36,19 @@ function HomePage() {
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
-      {loading && (
-        <div className="text-center text-primary py-10">Loading notes...</div>
-      )}
+      <div className="max-w-7xl mx-auto mt-6 p-4">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg: grid-cols-3 gap-6 ">
+            {notes.map((note,_id) => (
+              <NoteCard key={_id} note={note} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
