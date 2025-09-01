764b8608d6c20e114de9a0f43b9e8b06726a258b
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-09-01 23:21:18 +0300

    feat: enhance CreatePage with form handling and validation; update rate limiting configuration

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import { ArrowLeftIcon } from "lucide-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 function CreatePage() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title.trim() || !content.trim()) {
+      toast.error("Please provide all fields");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await axios.post("http://localhost:5001/api/notes", { title, content });
+      setLoading(false);
+      toast.success("Note created successfully");
+      navigate("/");
+    } catch (error) {
+      console.log("Error in creating note", error);
+      if (error.response.status === 429) {
+        toast.error("Slow down! You are creating too fast!", {
+          duration: 2000,
+          icon: "💣💣",
+        });
+      } else {
+        toast.error("Failed to create note");
+      }
+      setLoading(false);
+    }
+  };
   return (
-    <div>CreatePage</div>
-  )
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="size-5" />
+            <span className="text-xl">Back To Notes </span>
+          </Link>
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="text-lg">Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Note Title"
+                    className="input input-bordered"
+                    value={title}
+                    onChange={(e) => {
+                      setTitle(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="text-lg">Content</span>
+                  </label>
+                  <textarea
+                    type="text"
+                    placeholder="Write your note here..."
+                    className="textarea textarea-bordered h-32"
+                    value={content}
+                    onChange={(e) => {
+                      setContent(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="card-actions justify-end">
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    disabled={loading}
+                  >
+                    {loading ? "Creating..." : "Create Note"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CreatePage
+export default CreatePage;
