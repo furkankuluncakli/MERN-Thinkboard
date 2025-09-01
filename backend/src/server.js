fe0bdaa0ef973cc0bd60b471db991d16c130d4c0
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-09-01 17:42:37 +0300

    feat: add CORS support to the server and update package dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import router from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
+
+import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
@@ -12,6 +14,11 @@ dotenv.config();
 
 app.use(express.json());
 app.use(rateLimiter);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use("/api/notes", router);
 
