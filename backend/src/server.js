4da54683ec625200a149463c0a51cd689cbb20ab
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-09-02 13:43:24 +0300

    feat: enhance server configuration and improve axios base URL handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -9,18 +10,30 @@ import rateLimiter from "./middleware/rateLimiter.js";
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
